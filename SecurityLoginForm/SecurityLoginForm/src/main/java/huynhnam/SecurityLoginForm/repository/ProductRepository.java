27d037701ef3e6397bf7c9eb8030b3d0423a22bf
KevinHuynh <108676435+KevinHuynh101@users.noreply.github.com>
2023-06-07 15:58:28 +0700

    code java

--- SecurityLoginForm/SecurityLoginForm/src/main/java/huynhnam/SecurityLoginForm/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package huynhnam.SecurityLoginForm.repository;
-
-
-import huynhnam.SecurityLoginForm.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-}
